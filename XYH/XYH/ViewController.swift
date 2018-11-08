02bdb1cab748d3ee50ba5ed31d6e7af6dfd08c5c
20171105138 <40413094+20171105138@users.noreply.github.com>
2018-11-08 09:51:52 +0800

    增加新功能
    可以进行浮点数运算

---
+++ XYH/XYH/ViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ViewController.swift
+//  XYH
+//
+//  Created by 夏宇航 on 2018/9/27.
+//  Copyright © 2018年 夏宇航. All rights reserved.
+//
+
+import UIKit
+
+extension Double{
+    public func roundTo(places: Int) -> Double{
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+class ViewController: UIViewController {
+    var temp : Double = 0.0
+    var sum : Double = 0.0
+    var val = ""
+    
+    @IBOutlet weak var caculaterDisplay: UITextField!
+    
+    @IBAction func Button1(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"1"
+    }
+    
+    @IBAction func Button2(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"2"
+    }
+    
+    @IBAction func Button3(_ sender: Any) {
+         caculaterDisplay.text = caculaterDisplay.text!+"3"
+    }
+    
+    @IBAction func Button4(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"4"
+    }
+    
+    @IBAction func Button5(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"5"
+    }
+    
+    @IBAction func Button6(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"6"
+    }
+    
+    @IBAction func Button7(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"7"
+    }
+    
+    @IBAction func Button8(_ sender: Any) {
+       caculaterDisplay.text = caculaterDisplay.text!+"8"
+    }
+    
+    @IBAction func Button9(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"9"
+    }
+    
+    @IBAction func Button0(_ sender: Any) {
+        caculaterDisplay.text = caculaterDisplay.text!+"0"
+    }
+    
+    @IBAction func ButtonADD(_ sender: Any) {
+        temp = Double(caculaterDisplay.text!)!
+        val = "+"
+        caculaterDisplay.text = ""
+    }
+    
+    @IBAction func ButtonSUB(_ sender: Any) {
+        temp = Double(caculaterDisplay.text!)!
+        val = "-"
+        caculaterDisplay.text = ""
+        
+    }
+    
+    @IBAction func ButtonMUL(_ sender: Any) {
+        temp = Double(caculaterDisplay.text!)!
+        val = "*"
+        caculaterDisplay.text = ""
+    }
+    
+    @IBAction func ButtonDIV(_ sender: Any) {
+        temp = Double(caculaterDisplay.text!)!
+        val = "/"
+        caculaterDisplay.text = ""
+    }
+    
+    @IBAction func Point(_ sender: Any) {
+        temp = Double(caculaterDisplay.text!)!
+        val = "."
+        caculaterDisplay.text = caculaterDisplay.text!+"."
+    }
+    
+    @IBAction func ButtonSUM(_ sender: Any) {
+        print(val)
+        switch val{
+            case"+":
+            sum = temp+Double(caculaterDisplay.text!)!;
+            case"-":
+            sum = temp-Double(caculaterDisplay.text!)!;
+            case"*":
+            sum = temp*Double(caculaterDisplay.text!)!;
+            case"/":
+            sum = temp/Double(caculaterDisplay.text!)!;
+            default:
+                break;
+            }
+        caculaterDisplay.text = "\(sum.roundTo(places: 10))"
+        caculaterDisplay.text = "\(sum)"
+        
+        
+    }
+    
+    @IBAction func CLEAR(_ sender: Any) {
+        caculaterDisplay.text = ""
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
